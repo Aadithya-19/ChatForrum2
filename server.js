004e52e9337605c1d5aebd7c46e341cfdc0ec484
AVioli <68108339+Aadithya-19@users.noreply.github.com>
2022-10-19 19:00:56 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,18 +3,20 @@ const app = express();
 const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
 const io = require("socket.io")(server, {
     cors: {
         origin: '*'
     }
 });
+
 app.get("/", (req, res) => {
     res.render("index");
 });
 
 io.on("connection", (socket) => {
-    socket.on("message", (message) => {
-        io.emit("createMessage", message);
+    socket.on("message", (message, user) => {
+        io.emit("createMessage", message, user);
     });
 });
 
